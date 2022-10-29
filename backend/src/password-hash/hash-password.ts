bdbfc688e0e57672b7eee7188adc8737a1ddd54e
CruzedV <mr.dobrikv@mail.ru>
2022-10-29 12:59:33 +0300

    Prototype of routes structure

--- backend/src/password-hash/hash-password.ts
+++
@@ -1,6 +0,0 @@
-const bcrypt = require('bcrypt');
-
-export const hashPassword = async (password: string) => {
-    const salt = await bcrypt.genSalt(10);
-    return await bcrypt.hash(password, salt);
-    }
