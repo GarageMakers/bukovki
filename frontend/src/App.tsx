2d321e860f1222c00b6e3949352b497e47409430
swbonjour <71038541+swbonjour@users.noreply.github.com>
2022-10-30 00:42:06 +0300

    ghpages

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -17,14 +17,14 @@ function App() {
       <div className="content">
         <div className="wrapper">
           <Routes>
-            <Route path="/Sosedi" element={<LoginPage></LoginPage>}></Route>
-            <Route path="/Sosedi/profile" element={<PersonalPage></PersonalPage>}></Route>
-            <Route path="/Sosedi/chat" element={<ChatPage></ChatPage>}></Route>
-            <Route path="/Sosedi/appeal" element={<AppealPage></AppealPage>}></Route>
-            <Route path="/Sosedi/appeal/create" element={<CreateAppealPage></CreateAppealPage>}></Route>
-            <Route path="/Sosedi/UK/profile" element={<UKPersonalPage></UKPersonalPage>}></Route>
-            <Route path="/Sosedi/UK/appeal" element={<UKAppealPage></UKAppealPage>}></Route>
-            <Route path="/Sosedi/payments" element={<PaymentsPage></PaymentsPage>}></Route>
+            <Route path="/sosedi" element={<LoginPage></LoginPage>}></Route>
+            <Route path="/sosedi/profile" element={<PersonalPage></PersonalPage>}></Route>
+            <Route path="/sosedi/chat" element={<ChatPage></ChatPage>}></Route>
+            <Route path="/sosedi/appeal" element={<AppealPage></AppealPage>}></Route>
+            <Route path="/sosedi/appeal/create" element={<CreateAppealPage></CreateAppealPage>}></Route>
+            <Route path="/sosedi/uk/profile" element={<UKPersonalPage></UKPersonalPage>}></Route>
+            <Route path="/sosedi/uk/appeal" element={<UKAppealPage></UKAppealPage>}></Route>
+            <Route path="/sosedi/payments" element={<PaymentsPage></PaymentsPage>}></Route>
           </Routes>
         </div>
       </div>
