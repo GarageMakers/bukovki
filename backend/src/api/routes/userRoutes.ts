655f3f1320b6779d98c8afce075e3922a18da834
CruzedV <mr.dobrikv@mail.ru>
2022-10-29 17:17:58 +0300

    ORM 1.6

--- backend/src/api/routes/userRoutes.ts
+++ backend/src/api/routes/userRoutes.ts
@@ -1,16 +1,14 @@
 import dbConnection from "../../database/dbinit";
-import { Achievements, Appeals, ManagmentCompanies, Users, UsersGroups } from "../../database/Entities"
+import { achievements, appeals, managmentcompanies, users, usersgroups } from "../../database/Entities"
 const typeorm = require("typeorm")
 const express = require("express")
 const router = express.Router()
 
-const usersRepository = dbConnection.getRepository(Users);
+const usersRepository = dbConnection.getRepository(users);
 
 router.get("/", async (req, res) => {
-    // dbConnection.connect()
     const users = await usersRepository.find()
     res.send({userGroups: `${users}`})
-    // dbConnection.release()
 });
 
 module.exports = router;
