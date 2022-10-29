19d7ce9c711ba5c0614e52af3fecc859cdb4d4c4
Uzhastin-Nikita <fosfor20@mail.ru>
2022-10-29 14:31:25 +0300

    refactor: delete unnessesary tags

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,29 +1,19 @@
 import server from "./server";
 import dbConnection from "./dbinit";
-const dotenv = require("dotenv");
-
-dotenv.config()
+require('dotenv').config();
 
 const startServer = async () => {
-  try {
+
     dbConnection()
     console.log(
-      `###################################\nDataBase connected on port: ${process.env.PGPORT}\n###################################`
+      `\nDataBase connected on port: ${process.env.PGPORT}\n`
     )
-  } catch(err) {
-    console.log("Errot in app.ts WITH dbconnection")
-  }
-  try {
     const app = await server();
     app?.listen(process.env.EXPPORT, () => {
       console.log(
-        `###################################\nServer listening on port: ${process.env.EXPPORT}\n###################################`
+        `\nServer listening on port: ${process.env.EXPPORT}\n`
       )
     });
-  } catch (err) {
-    console.log("Error in app.ts WITH server")
-    console.log(err)
-  }
 };
 
 startServer();
--- backend/src/dbinit.ts
+++ backend/src/dbinit.ts
@@ -1,25 +1,28 @@
-import dotenv from "dotenv";
+const dotenv = require("dotenv");
 const typeorm = require("typeorm");
 import { Achievements, Appeals, ManagmentCompanies, Users, UsersGroups } from "./database/Entities";
 
 dotenv.config();
 
 console.log("dbinit.ts is running")
-const dbConnection = () => {
+function dbConnection () {
 (async () => {
     // Initialize a connection pool against the database.
+    try {
+      console.log("dbinit.ts is trying to connect to the database")
     const connection = await typeorm.createConnection({
       type: "postgres",
       host: process.env.PGHOST,
-      port: parseInt(process.env.PGPORT, 10),
+      port: "5432",
       username: process.env.PGUSER,
       password: process.env.PGPASSWORD,
       database: process.env.PGDATABASE,
       entities: [ Users, ManagmentCompanies, UsersGroups, Appeals, Achievements ], 
     });
-    console.log(connection);
+  } catch (error) {
+    console.log("Error in dbinit.ts", error)
+  }
 });
 };
 
-dbConnection();
 export default dbConnection;
