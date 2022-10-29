bc9019d2b4dc9c9c3d5528c817913b1f0e4d6fdf
Uzhastin-Nikita <fosfor20@mail.ru>
2022-10-29 13:28:55 +0300

    feat: add typeorm to const

--- backend/src/dbinit.ts
+++ backend/src/dbinit.ts
@@ -1,13 +1,14 @@
 import dotenv from "dotenv";
-import { createConnection } from "typeorm";
+const typeorm = require("typeorm");
 import { Achievements, Appeals, ManagmentCompanies, Users, UsersGroups } from "./database/Entities";
 
 dotenv.config();
 
+console.log("dbinit.ts is running")
 const dbConnection = () => {
 (async () => {
     // Initialize a connection pool against the database.
-    const connection = await createConnection({
+    const connection = await typeorm.createConnection({
       type: "postgres",
       host: process.env.PGHOST,
       port: parseInt(process.env.PGPORT, 10),
@@ -16,7 +17,9 @@ const dbConnection = () => {
       database: process.env.PGDATABASE,
       entities: [ Users, ManagmentCompanies, UsersGroups, Appeals, Achievements ], 
     });
+    console.log(connection);
 });
 };
 
+dbConnection();
 export default dbConnection;
